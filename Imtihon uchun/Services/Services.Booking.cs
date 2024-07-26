ca4ba7d56eeb8a42937832e6b1719607ffeeebe3
diyor5656 <absalomovdiyorbek0@gmail.com>
2024-07-26 22:30:29 +0500

    2-uzgarish

--- Imtihon uchun/Services/Services.Booking.cs
+++ Imtihon uchun/Services/Services.Booking.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Imtihon_uchun.Services
 {
@@ -16,16 +17,34 @@ namespace Imtihon_uchun.Services
             currientPath += @"\arizalar.json";
             return currientPath;
         }
-        public void ClearCFile()
+        public void ClearAFile()
         {
             Console.Clear();
-            File.WriteAllText(GetCPath(), string.Empty);
+            File.WriteAllText(GetAPath(), string.Empty);
             Console.WriteLine("Arizalar Tozalandi! ");
         }
-        public void SaveC(List<Booking> bookings)
+        public void SaveB(List<Booking> bookings)
         {
             string serialized = JsonSerializer.Serialize(bookings);
             File.WriteAllText(GetBookPath(), serialized);
         }
+        public void AddBooking()
+        {
+            Console.Clear();
+            List<Booking> bookings = new List<Booking>();
+            Console.Write("Kitob Nomini kiriting: ");
+            string bookname = Console.ReadLine();
+            int newId = bookings.Count > 0 ? bookings.Max(t => t.Id) + 1 : 1;
+            Booking boking = new Booking()
+            {
+                Id = newId,
+                Name = bookname,
+
+            };
+            bookings.Add(boking);
+            SaveB(bookings);
+            Console.WriteLine("Ariza Muvaffaqiyatli qo'Shildi! ");
+        }
+
     }
 }
